e40c3ec92a87b05b0b643cb5c29388e3d6c703d6
proiacm <proiacm@gmail.com>
2020-09-14 17:40:03 -0600

    rewrite event listener to toggle

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-console.log("testing")
+// console.log("testing")
 
-document.getElementById("menu").addEventListener("click", () => console.log("clicked"))
+document.getElementById("menu").addEventListener("click", handleMenuClick)
+
+function handleMenuClick() {
+    let nav = document.getElementById("nav")
+        if (nav.style.display === "none") {
+            nav.style.display = "block"
+        } else {
+            nav.style.display = "none"
+        }
+}
